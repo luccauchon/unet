c88a3f300445de7d3f51dd6d645d38163bd27cce
CJ3272 <cauchon.luc@ireq.ca>
2019-03-15 10:57:07 -0400

    Lâche pas mon Gérard.

--- train_AMATEUR.py
+++ train_AMATEUR.py
@@ -6,13 +6,13 @@ from model import *
 from luccauchon.data.Generators import AmateurDataFrameDataGenerator
 import luccauchon.data.Generators as generators
 
-df_train, df_val = generators.amateur_train_val_split('G:/AMATEUR/segmentation/22FEV2019/GEN_segmentation/', number_elements=None)
+df_train, df_val = generators.amateur_train_val_split('G:/AMATEUR/segmentation/22FEV2019/GEN_segmentation/', number_elements=16)
 
 dim_image = (256, 256, 3)
 model = unet(input_size=dim_image)
 
-train_generator = AmateurDataFrameDataGenerator(df_train, batch_size=4, img_w=dim_image[0], img_h=dim_image[1])
-val_generator = AmateurDataFrameDataGenerator(df_val, batch_size=4, img_w=dim_image[0], img_h=dim_image[1])
+train_generator = AmateurDataFrameDataGenerator(df_train, batch_size=4, dim_image=dim_image)
+val_generator = AmateurDataFrameDataGenerator(df_val, batch_size=4, dim_image=dim_image)
 
 model.fit_generator(generator=train_generator, steps_per_epoch=None, epochs=10, verbose=1, callbacks=None,
                     validation_data=val_generator, validation_steps=None, class_weight=None, max_queue_size=10,
