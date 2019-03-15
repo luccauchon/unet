0a38085ea7fcef536f449e3225f41df48f882628
CJ3272 <cauchon.luc@ireq.ca>
2019-03-15 09:04:02 -0400

    Lâche pas mon Gérard.

--- train_AMATEUR.py
+++ train_AMATEUR.py
@@ -4,16 +4,16 @@ os.environ['basedir_a'] = 'C:/temp2/'
 
 from model import *
 from luccauchon.data.Generators import AmateurDataFrameDataGenerator
-from luccauchon.data.Generators import TrainDecoratorAmateurDataFrameDataGenerator
-from luccauchon.data.Generators import ValDecoratorAmateurDataFrameDataGenerator
+import luccauchon.data.Generators as generators
 
-import luccauchon.data.C as C
+df_train, df_val = generators.amateur_train_val_split('G:/AMATEUR/segmentation/22FEV2019/GEN_segmentation/', number_elements=None)
 
-df = C.generate_X_y_information_from_via_dataset('G:/AMATEUR/segmentation/22FEV2019/GEN_segmentation/', number_elements=64)
 dim_image = (256, 256, 3)
 model = unet(input_size=dim_image)
 
-generator = AmateurDataFrameDataGenerator(df, epochs=10, batch_size=4, img_w=dim_image[0], img_h=dim_image[1])
-model.fit_generator(generator=TrainDecoratorAmateurDataFrameDataGenerator(generator), steps_per_epoch=None, epochs=10, verbose=1, callbacks=None,
-                    validation_data=ValDecoratorAmateurDataFrameDataGenerator(generator), validation_steps=None, class_weight=None, max_queue_size=10,
+train_generator = AmateurDataFrameDataGenerator(df_train, batch_size=4, img_w=dim_image[0], img_h=dim_image[1])
+val_generator = AmateurDataFrameDataGenerator(df_val, batch_size=4, img_w=dim_image[0], img_h=dim_image[1])
+
+model.fit_generator(generator=train_generator, steps_per_epoch=None, epochs=10, verbose=1, callbacks=None,
+                    validation_data=val_generator, validation_steps=None, class_weight=None, max_queue_size=10,
                     workers=1, use_multiprocessing=False, shuffle=True, initial_epoch=0)
