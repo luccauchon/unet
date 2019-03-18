3abe4b8ba5dce2a79f6453b0ccc94d95ce38c90d
cj3272 <luccauchon@gmail.com>
2019-03-18 13:45:20 -0400

    Lache pas mon Gerard.

--- model.py
+++ model.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import numpy as np
-import os
-import skimage.io as io
-import skimage.transform as trans
-import numpy as np
-=======
->>>>>>> refs/remotes/origin/master
 from keras.models import *
 from keras.layers import *
 from keras.optimizers import *
